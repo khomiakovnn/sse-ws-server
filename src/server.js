1c272fee79c1a7e08f3c48b497b1d11b134c8106
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2024-01-16 13:50:03 -0500

    done

--- src/server.js
+++ src/server.js
@@ -1,35 +1,56 @@
 const WebSocket = require('ws');
 const http = require('http');
 
-// Создаем HTTP-сервер
+let clients = [];
+let usersOnline = []
+const messages = [];
+
+function sendAll() {
+  const message = JSON.stringify([usersOnline, messages])
+  clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
+}
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('WebSocket server is running!\n');
 });
 
-// Создаем WebSocket-сервер на основе HTTP-сервера
 const wss = new WebSocket.Server({ server });
 
-// Обработчик подключения нового клиента
 wss.on('connection', (ws) => {
   console.log(`Client connected from IP ${ws._socket.remoteAddress} and port ${ws._socket.remotePort}`);
+  clients.push(ws);
+  let standingUser = '';
+  sendAll();
 
-  // Обработчик сообщений от клиента
   ws.on('message', (message) => {
     console.log(`Received message from client at port ${ws._socket.remotePort}: ${message}`);
-    ws.send(`Server received your message: ${message}`);
+    const receivedData = JSON.parse(message);
 
-    // Отправляем сообщение обратно клиенту
-    ws.send(`Server received your message: ${message}`);
+    switch (receivedData[0]) {
+        case 'new user':
+          standingUser = receivedData[1]
+          usersOnline.push(standingUser);
+          break;
+        case 'new message':
+          messages.push(receivedData[1]);
+          break;
+    }
+    sendAll();
   });
 
-  // Обработчик закрытия соединения
   ws.on('close', () => {
-    console.log(`Client at port ${ws._socket.remotePort} disconnected`);
+    console.log(`Client from IP ${ws._socket.remoteAddress} and port ${ws._socket.remotePort} disconnected`);
+    clients = clients.filter(item => item !== ws);
+    usersOnline = usersOnline.filter(item => item !== standingUser);
+    sendAll();
   });
 });
 
-// Запускаем сервер на порту 3000
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`WebSocket server is running on port ${PORT}`);
